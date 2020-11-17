55d548dc910f9266ccaf634883c557bf3b9b6c4f
Helmut Josue Colindres Brenes <helmut-1521@hotmail.com>
2020-11-17 14:02:08 -0600

    Diseño de activity para Facturacion

--- app/src/main/java/com/example/myapplication/MainProductosCliente.java
+++ app/src/main/java/com/example/myapplication/MainProductosCliente.java
@@ -5,12 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 public class MainProductosCliente extends AppCompatActivity implements View.OnClickListener {
 ImageButton IbuttomBuscar,IbuttonInicio,IbuttonAgregar,IbuttonSiguiente;
-TextView textcontar,textprecio;
+TextView textcontar,textinfo1,textinfo2,textinfo3,textinfo4,textinfo5;
+Spinner precios,monedas;
     @Override
     protected void onCreate(Bundle savedInstanceState)  {
         super.onCreate(savedInstanceState);
@@ -19,15 +22,39 @@ TextView textcontar,textprecio;
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle("Productos");
 
+        ///////// Botones
         IbuttomBuscar = findViewById(R.id.btn_buscar);
         IbuttonInicio = findViewById(R.id.btn_Inicio);
         IbuttonAgregar = findViewById(R.id.btn_Agregar);
         IbuttonSiguiente = findViewById(R.id.btn_siguente);
 
+        /////////// campos de texto
+        textcontar=findViewById(R.id.text_contar);
+        textinfo1=findViewById(R.id.text_info1);
+        textinfo2=findViewById(R.id.text_info2);
+        textinfo3=findViewById(R.id.text_info3);
+        textinfo4=findViewById(R.id.text_info4);
+        textinfo5=findViewById(R.id.text_info5);
+
+        ////////// Spinmer
+        precios = findViewById(R.id.spinerPrecios);
+        monedas = findViewById(R.id.spinner_tipo_moneda);
+
         IbuttomBuscar.setOnClickListener(this);
         IbuttonInicio.setOnClickListener(this);
         IbuttonAgregar.setOnClickListener(this);
         IbuttonSiguiente.setOnClickListener(this);
+
+
+        /////////Spinner del tipo de moneda
+        ArrayAdapter<CharSequence> adapter  = ArrayAdapter.createFromResource(this, R.array.tipo_moneda, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        monedas.setAdapter(adapter);
+
+        ////////////spinner de los precios
+        ArrayAdapter<CharSequence> adapter1 =ArrayAdapter.createFromResource(this, R.array.precios, android.R.layout.simple_spinner_item);
+        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        precios.setAdapter(adapter1);
     }
 
     @Override
@@ -52,8 +79,4 @@ TextView textcontar,textprecio;
                 break;
         }
     }
-
-   public void OpenDialog(){
-
-   }
 }
