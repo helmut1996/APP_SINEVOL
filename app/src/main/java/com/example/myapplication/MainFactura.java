bd2c6a44c6dc69e0f95914f96ecdba1988ceefd9
Helmut josue colindres brenes <helmut-1521@hotmail.com>
2020-11-26 11:35:36 -0600

    Mostrando Datos cliente Recycle view funcional

--- app/src/main/java/com/example/myapplication/MainFactura.java
+++ app/src/main/java/com/example/myapplication/MainFactura.java
@@ -1,11 +1,15 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
@@ -17,11 +21,15 @@ import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.SearchView;
+import android.widget.Toast;
 
 import com.example.myapplication.Adapter.RecycleviewAdapter;
 import com.example.myapplication.ConexionBD.DBConnection;
 import com.example.myapplication.modelos.ClasslistItemC;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -40,6 +48,11 @@ public class MainFactura extends AppCompatActivity  {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_factura);
 
+
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.INTERNET}, PackageManager.PERMISSION_GRANTED);
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle("Clientes");
 
@@ -57,10 +70,31 @@ public class MainFactura extends AppCompatActivity  {
         LinearLayoutManager manager= new LinearLayoutManager(this);
         recyclerViewCliente.setLayoutManager(manager);
 
-        itemCList=getItemCList();
+        itemCList=obtenerclientesBD();
         adapterCliente= new RecycleviewAdapter(itemCList);
         recyclerViewCliente.setAdapter(adapterCliente);
     }
+
+private List<ClasslistItemC>obtenerclientesBD(){
+        List<ClasslistItemC>listCliiente= new ArrayList<>();
+        try {
+            DBConnection dbConnection = new DBConnection();
+            dbConnection.conectar();
+
+            Statement st=dbConnection.getConnection().createStatement();
+            ResultSet rs=st.executeQuery("select Codigo,Nombre,Direccion,DireccionHabitacion,LimiteCredito from Clientes");
+            while(rs.next()){
+
+                listCliiente.add(new ClasslistItemC(rs.getString("Nombre"),rs.getString("Direccion"),rs.getString("DireccionHabitacion"),rs.getInt("Codigo"),rs.getInt("LimiteCredito")));
+
+            }
+        } catch (SQLException e) {
+            Toast.makeText(getApplicationContext(),e.getMessage(),Toast.LENGTH_SHORT).show();
+            e.printStackTrace();
+        }
+
+        return listCliiente;
+}
 
     private List<ClasslistItemC>getItemCList(){
         List<ClasslistItemC> itemslistcliente= new ArrayList<>();
