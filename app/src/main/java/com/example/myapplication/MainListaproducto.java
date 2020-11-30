daf8ce30320329e46d8755f3f011e60e416d1e64
Helmut josue colindres brenes <helmut-1521@hotmail.com>
2020-11-30 11:34:13 -0600

    Mostrando listado Productos BD en Recycle view

--- app/src/main/java/com/example/myapplication/MainListaproducto.java
+++ app/src/main/java/com/example/myapplication/MainListaproducto.java
@@ -1,11 +1,15 @@
-package com.example.myapplication;
+    package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
@@ -14,14 +18,20 @@ import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.example.myapplication.Adapter.RecycleviewAdapter;
 import com.example.myapplication.Adapter.RecycleviewProductoAdapter;
+import com.example.myapplication.ConexionBD.DBConnection;
 import com.example.myapplication.modelos.ModelItemsProducto;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.List;
 
-public class MainListaproducto extends AppCompatActivity {
+    public class MainListaproducto extends AppCompatActivity {
 
     EditText editBuscar;
     RecyclerView recyclerlistproducto;
@@ -31,6 +41,12 @@ ArrayList<ModelItemsProducto> listaProducto;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        /// permisoos de conexion para uso de sql server ////
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.INTERNET}, PackageManager.PERMISSION_GRANTED);
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        /// permisoos de conexion para uso de sql server ////
+
         setContentView(R.layout.activity_main_listaproducto);
         getSupportActionBar().setTitle("Lista de Productos");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
@@ -39,12 +55,8 @@ ArrayList<ModelItemsProducto> listaProducto;
         listaProducto=new ArrayList<>();
         editBuscar= findViewById(R.id.edit_Buscar);
         recyclerlistproducto= findViewById(R.id.list_producto);
-        recyclerlistproducto.setLayoutManager(new LinearLayoutManager(this));
 
-        llenarProductos();
-
-        RecycleviewProductoAdapter adapter=new RecycleviewProductoAdapter(listaProducto);
-        recyclerlistproducto.setAdapter(adapter);
+        initVlaues();
 /*
         adadter =new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,getResources().getStringArray(R.array.Productos));
         listproducto.setAdapter(adadter);
@@ -97,13 +109,48 @@ ArrayList<ModelItemsProducto> listaProducto;
 
     }
 
+
+    private List<ModelItemsProducto> llenarProductosBD(){
+        List<ModelItemsProducto> listProducto = new ArrayList<>();
+        try {
+
+            DBConnection dbConnection = new DBConnection();
+            dbConnection.conectar();
+
+            Statement st = dbConnection.getConnection().createStatement();
+            ResultSet rs = st.executeQuery("\n" +
+                    "\n" +
+                    "select concat(i.Nombre, ' C$ ', i.Precio1,' ',um.Nombre) as Nombre,um.Nombre as UM, i.Precio1 from Inventario i inner join Unidad_Medida um on i.idUndMedida=um.idUnidadMedida");
+
+            while (rs.next()){
+
+
+
+                listProducto.add(new ModelItemsProducto(rs.getString("Nombre"),rs.getString("UM"),rs.getDouble("Precio1") ));
+            }
+        } catch (SQLException e) {
+            Toast.makeText(getApplicationContext(),e.getMessage(),Toast.LENGTH_SHORT).show();
+            e.printStackTrace();
+        }
+        return listProducto;
+    }
+
+    private void initVlaues(){
+        recyclerlistproducto.setLayoutManager(new LinearLayoutManager(this));
+        RecycleviewProductoAdapter adapter=new RecycleviewProductoAdapter(listaProducto);
+      listaProducto= (ArrayList<ModelItemsProducto>) llenarProductosBD();
+      adapter= new RecycleviewProductoAdapter(listaProducto);
+        recyclerlistproducto.setAdapter(adapter);
+
+    }
+
     private void llenarProductos() {
-            listaProducto.add(new ModelItemsProducto("Lapiz Azul","1/10",152.50));
-            listaProducto.add(new ModelItemsProducto("Lapiz Azul","1/10",152.50));
-            listaProducto.add(new ModelItemsProducto("Lapiz Azul","1/10",152.50));
-            listaProducto.add(new ModelItemsProducto("Lapiz Azul","1/10",152.50));
-            listaProducto.add(new ModelItemsProducto("Lapiz Azul","1/10",152.50));
-            listaProducto.add(new ModelItemsProducto("Lapiz Azul","1/10",152.50));
+            listaProducto.add(new ModelItemsProducto("Lapiz Azul Helmut Colindres","1/10",152.50));
+            listaProducto.add(new ModelItemsProducto("Lapiz Azul helmut Colindres","1/10",152.50));
+            listaProducto.add(new ModelItemsProducto("Lapiz Azul helkmut","1/10",152.50));
+            listaProducto.add(new ModelItemsProducto("Lapiz Azul helmut","1/10",152.50));
+            listaProducto.add(new ModelItemsProducto("Lapiz Azul helmut","1/10",152.50));
+            listaProducto.add(new ModelItemsProducto("Lapiz Azul helmut colindres","1/10",152.50));
     }
 
 
