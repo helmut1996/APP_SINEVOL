49f1836561ddb80cf92941d5c457229f8e56091b
Helmut josue colindres brenes <helmut-1521@hotmail.com>
2020-12-30 16:01:43 -0600

    Diseño de Recibo

--- app/src/main/java/com/example/myapplication/MainRecibo.java
+++ app/src/main/java/com/example/myapplication/MainRecibo.java
@@ -3,8 +3,11 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.AutoCompleteTextView;
+import android.widget.EditText;
 
 public class MainRecibo extends AppCompatActivity {
+AutoCompleteTextView buscadorCliente,BuscadorFactura;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
