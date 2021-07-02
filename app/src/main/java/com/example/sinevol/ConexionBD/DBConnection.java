c5ebe4aba77db1cc27ceff77fc9dca344bc53b28
helmut josue colindres Brenes <helmut-1521@hotmail.com>
2021-07-02 13:54:58 -0600

    Cambio

--- app/src/main/java/com/example/sinevol/ConexionBD/DBConnection.java
+++ app/src/main/java/com/example/sinevol/ConexionBD/DBConnection.java
@@ -6,11 +6,12 @@ import java.sql.SQLException;
 
 public class DBConnection
 {
-    private static String ip = "161.0.62.5";
-    //private static String ip = "192.168.0.100";
+
+    //private static String ip = "161.0.62.5";
+    private static String ip = "192.168.0.100";
     private static String port = "1440"; 
     private static String Classes = "net.sourceforge.jtds.jdbc.Driver";
-    private static String database = "BDMARNOR";
+    private static String database = "BDPRUEBA2";
     private static String username = "sa";
     private static String password = "System32";
     private Connection connection = null;
